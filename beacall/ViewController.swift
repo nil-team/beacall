d56d4d7d0624c818979adf8fa92b7568175d6e9e
Arnaud Manaranche <arnaud.mana@gmail.com>
2017-12-20 11:39:04 +0100

    Initial Commit

---
+++ beacall/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  beacall
+//
+//  Created by Arnaud Manaranche on 20/12/2017.
+//  Copyright © 2017 ArnaudManaranche. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
